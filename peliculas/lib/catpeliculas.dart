fe56cab5b58fae15610e350fe6a73ee2f8258359
andreiiGP <andru0829@gmail.com>
2023-10-16 07:43:11 -0500

    realice un slider de peliculas

--- peliculas/lib/catpeliculas.dart
+++ peliculas/lib/catpeliculas.dart
@@ -1,74 +1,114 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Movie {
-  final String imageUrl;
+void main() => runApp(MyApp());
 
-  Movie( this.imageUrl);
-}
-
-class peli extends StatelessWidget {
-  final List<Movie> movies = [
-    Movie('assets/spiderman.jpg'),
-    Movie('https://image.tmdb.org/t/p/w342/lrZyrYCWiQO8ljwhH9sX5RlZN5l.jpg'),
-    Movie('assets/spiderman.jpg'),
-    // Agrega más películas aquí
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Catálogo de Películas',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Catálogo de Películas'),
-        ),
-        body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // Puedes ajustar este valor según tu preferencia
-          ),
-          itemCount: movies.length,
-          itemBuilder: (context, index) {
-            return MovieTile(movie: movies[index]);
-          },
-        ),
-      ),
+      home: peli(),
     );
   }
 }
 
-class MovieTile extends StatelessWidget {
-  final Movie movie;
+class peli extends StatefulWidget {
+  @override
+  _ImageSliderState createState() => _ImageSliderState();
+}
 
-  const MovieTile({required this.movie});
+class _ImageSliderState extends State<peli> {
+  final List<String> imageList = [
+    'assets/spidermanSlider.jpeg',
+    'assets/soulSlider.jpg',
+    'assets/guardianesSlider.jpg',
+    'assets/deadpoolSlider.jpg',
+    'assets/timeistheenemi.jpeg',
+    // Agrega más rutas de imágenes según sea necesario
+  ];
+
+  int _currentIndex = 0;
+  late PageController _pageController;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController(initialPage: _currentIndex);
+
+    // Configura un temporizador para desplazar automáticamente las imágenes
+    _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (_currentIndex < imageList.length - 1) {
+        _currentIndex++;
+      } else {
+        _currentIndex = 0;
+      }
+      _pageController.animateToPage(
+        _currentIndex,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 114, // Ajusta la altura de la tarjeta según tu preferencia
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.network(
-                    movie.imageUrl,
-                    fit: BoxFit.cover,
-                    height: 115,
-                    width: 400,
-                  ),
-                  // Agrega más contenido si es necesario
-                ],
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300, // Altura del slider
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: imageList.length,
+              itemBuilder: (context, index) {
+                return Image.asset(
+                  imageList[index],
+                  fit: BoxFit.cover,
+                );
+              },
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _buildPageIndicator(),
           ),
         ],
       ),
     );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < imageList.length; i++) {
+      indicators.add(
+        Container(
+          width: 8.0,
+          height: 8.0,
+          margin: EdgeInsets.symmetric(horizontal: 4.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentIndex == i ? Colors.blue : Colors.grey,
+          ),
+        ),
+      );
+    }
+    return indicators;
   }
 }
